5bffd431d2f2dc97ca493f16ab4e026ccd329a3c
Arman Mielke <arman.mielke@yahoo.de>
2021-03-07 02:03:17 +0100

    Improve logging

--- src/log.py
+++ src/log.py
@@ -1,4 +1,23 @@
+from torch import Tensor
 
-def print_log(message: str):
-    # TODO proper logging
-    print(message)
+import os
+from typing import Callable, Tuple
+
+
+LOG_FILE_NAME = "log.txt"
+
+
+def initialise_logging(log_dir: str) -> Tuple[Callable[[str], None], Callable[[Tensor], None]]:
+    os.makedirs(log_dir)
+
+    def log(message: str):
+        """Logs a message by printing it and appending it to a log file."""
+        print(message)
+        with open(os.path.join(log_dir, LOG_FILE_NAME), 'a+') as log_file:
+            log_file.write(message + '\n')
+
+    def log_image(image: Tensor):
+        # TODO
+        pass
+
+    return log, log_image
--- src/train.py
+++ src/train.py
@@ -2,27 +2,29 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 
-from typing import Callable
+from typing import Callable, Optional
 
 from datasets import load_flowers_dataset
-from log import print_log
+from log import initialise_logging
 from models import BasicCNN
 
 
 def train(
+    log_dir: str,
     epochs: int = 1000,
     # TODO use schedule
     lr: float = 5e-6,
     batch_size: int = 64,
     use_cuda: bool = True,
-    log: Callable[[str], None] = print_log,
+    log_images_after_epochs: Optional[int] = 10
 ):
+    log, log_images = initialise_logging(log_dir)
     train_loader = DataLoader(load_flowers_dataset(), batch_size, shuffle=True, num_workers=2, drop_last=True)
     log("Dataset loaded.")
     model = BasicCNN()
     optimiser = Adam(model.parameters(), lr)
 
-    for epoch in range(epochs):
+    for epoch in range(1, epochs + 1):
         loss_sum = 0
 
         for images in train_loader:
@@ -41,8 +43,11 @@ def train(
         log(f"[Epoch {epoch}] Train loss: {loss_mean}")
 
         # TODO use test set
-        # TODO visualise and log images
+
+        if log_images_after_epochs is not None and epoch % log_images_after_epochs == 0:
+            # TODO log images
+            pass
 
 
 if __name__ == "__main__":
-    train(use_cuda=False)
+    train("../log/", use_cuda=False)
